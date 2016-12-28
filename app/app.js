678c6211fce81888bc3fdbed261afee7788a65ec
Nirmal Kumar V <vnknirmalkumar@gmail.com>
2016-12-29 02:28:03 +0800

    daybook changes

--- app/app.js
+++ app/app.js
@@ -1,19 +1,59 @@
-angular
-	.module('daybook',[])
-	.controller('activityController',['$scope','$http',function($scope,$http){
-	console.log("controller loaded...");
-	
-		$('.dropdown-content li').click(function(eObj){
-        console.log(eObj.currentTarget);
-        var selectedVal = $(eObj.currentTarget).text();
-        var targetElement = $(eObj.currentTarget).parent().siblings('a');
+angular.module('daybook',[])
+.controller('activityController',['$scope','$http',function($scope,$http){
+    console.log("controller loaded...");
 
-        $(targetElement).text(selectedVal);
-    });
-		
-		
-		$scope.activities = [1,2,3,4,5];
-		
-		
+
+    
+    $scope.getCurrentDate = function(){
+        return (new Date()).getTime();
+    }
+
+    $scope.init = function(){
+        
+        $scope.categories = ['Spent','Received'];
+        $scope.for_types = ['Food','Groceries','Transport','Utility','Household','Lifestyle','Medical','Gift','Misc'];
+        $scope.payment_types = ['Cash','Card','Other'];
+
+        $scope.activity = {
+            "timestamp": new Date(),
+            "category": "Spent",
+            "amount": undefined,
+            "for": "Food",
+            "in": "",
+            "by": "Cash",
+            "at": "",
+        };
+
+
+        $scope.activities = [];
+        $http.get('/api/activity').then(function(response){
+            $scope.activities = response.data;
+        }, function(err){});
+    }
+	
+	$scope.addActivity = function(){
+        if($scope.activity && $scope.activity.amount){
+            var activity = angular.copy($scope.activity);
+            activity.timestamp = $scope.activity.timestamp.getTime();
+            $http.post('/api/activity/add',activity).then(function(response){        
+                $http.get('/api/activity').then(function(response){
+                    $scope.activities = response.data;
+
+                    $scope.activity = {
+                        "timestamp": new Date(),
+                        "category": "Spent",
+                        "amount": undefined,
+                        "for": "Food",
+                        "in": "",
+                        "by": "Cash",
+                        "at": "",
+                    };
+
+                }, function(err){});
+            }, function(err){});
+
+
+        }
+    }
 		
 }]);
--- index.js
+++ index.js
@@ -10,63 +10,118 @@ app.set('port', (process.env.PORT || 9000));
 
 db.connect('data/', ['activity']); // connect db and load collection
 
-app.use(express.static(__dirname));
+// mongoose 4.3.x
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname + '/index.html'));
+var ActivitySchema = new Schema({
+	"timestamp":Number,
+	"category":String,
+	"amount":Number,
+	"for":String,
+	"in":String,
+	"by":String,
+	"at":String
 });
 
-var router = express.Router();              // get an instance of the express Router
+var Activity = mongoose.model('Activity',ActivitySchema);
 
-router.get('/activity', function(req, res) {
-    var result = db.activity.find();   
-    res.json(result || {})
-});
 
-router.get('/activity/:id', function(req, res) {
-    var result = db.activity.findOne({_id:req.params.id});
-    res.json(result || {});
-});
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };       
+var mongodbUri = 'mongodb://heroku_zjzb59nk:llsgih1ktiqta1h77n5dg11vkr@ds145138.mlab.com:45138/heroku_zjzb59nk';
+mongoose.connect(mongodbUri, options);
+var conn = mongoose.connection;             
+conn.on('error', console.error.bind(console, 'connection error:'));  
+conn.once('open', function() {
+  // Wait for the database connection to establish, then start the app.       
 
-router.post('/activity/add', function(req, res) {
-    var result  = db.activity.save(req.body);
-    res.json({"status":"success","data":result});   
-});
+    console.log("Mongodb connection established...");   
 
-router.post('/activity/update',function(req, res){
-    var data = req.body;
-    var result  = db.activity.update({_id:data._id},data,{multi:false,upsert:true});
-    res.json({"status":"success","data":result});  
-});
+    app.use(express.static(__dirname));
+    app.use(bodyParser.urlencoded());
+    app.use(bodyParser.json());
 
-router.post('/activity/update/:id',function(req, res){
-    var data = req.body;
-    var result  = db.activity.update({_id:req.params.id || data._id},data,{multi:false,upsert:true});
-    res.json({"status":"success","data":result});  
-});
+    app.get('/', function(request, response) {
+        response.sendFile(path.join(__dirname + '/index.html'));
+    });
 
-router.post('/activity/delete',function(req, res){
-    var data = req.body;
-    var result = db.activity.remove({_id:data._id},false);
-    res.json({"status":"success","data":result})
-});
+    var router = express.Router();              // get an instance of the express Router
 
-router.post('/activity/delete/:id',function(req, res){
-    var data = req.body;
-    var result = db.activity.remove({_id:req.params.id || data._id},false);
-    res.json({"status":"success","data":result})
-});
+    router.get('/activity', function(req, res) {
+        Activity.find().sort('-timestamp').exec((err, activities) => {
+            if (err) return console.error(err);
+            console.log(activities);
+            res.json(activities || []);
+        })
+    });
 
-app.use('/api',router);
+    router.get('/activity/:id', function(req, res) {
+        Activity.findOne({_id:req.params.id}).exec((err,activity)=>{
+            res.json(activity || {});
+        });
+    });
 
-// middleware to redirect to index.html for any request for handle client side routing
-// app.use(function(request, response) {
-//   response.sendFile(path.join(__dirname + '/app/index.html'));
-// });
+    router.post('/activity/add', function(req, res) {
+        var activity = new Activity(req.body);
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+        activity.save(function (err) {
+            if (err) {
+                console.log("error");
+                return console.error(err);
+            }
+            console.log("activity saved!");
+
+            // var result  = db.activity.save(req.body);
+            res.json({"status":"success"});  
+        });
+ 
+    });
+
+    // router.post('/activity/update',function(req, res){
+    //     var data = req.body;
+    //     var result  = db.activity.update({_id:data._id},data,{multi:false,upsert:true});
+    //     res.json({"status":"success","data":result});  
+    // });
+
+    // router.post('/activity/update/:id',function(req, res){
+    //     var data = req.body;
+    //     var result  = db.activity.update({_id:req.params.id || data._id},data,{multi:false,upsert:true});
+    //     res.json({"status":"success","data":result});  
+    // });
+
+    // router.post('/activity/delete',function(req, res){
+    //     var data = req.body;
+    //     var result = db.activity.remove({_id:data._id},false);
+    //     res.json({"status":"success","data":result})
+    // });
+
+    // router.post('/activity/delete/:id',function(req, res){
+    //     var data = req.body;
+    //     var result = db.activity.remove({_id:req.params.id || data._id},false);
+    //     res.json({"status":"success","data":result})
+    // });
+
+    app.use('/api',router);
+
+    // middleware to redirect to index.html for any request for handle client side routing
+    // app.use(function(request, response) {
+    //   response.sendFile(path.join(__dirname + '/app/index.html'));
+    // });
+
+    app.listen(app.get('port'), function() {
+        console.log('Node app is running on port', app.get('port'));
+    });
+
+}.bind(this));
+
+// Mongoose connection
+
+
+
+
+
+
 
 
 
